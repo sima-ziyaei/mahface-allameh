c42c411acfaf90750b085f05536ea5935658efe4
sobhan <sobhandanger81@gmail.com>
2024-11-15 16:38:43 +0330

    HomePage added

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface HomePageProps {}
+
+const HomePage: FC<HomePageProps> = ({}) => {
+  return <div className="w-full h-full">HomePage</div>;
+};
+
+export default HomePage;
